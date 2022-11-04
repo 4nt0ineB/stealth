45fbb98b8d313a2b94817f1d408195149bb1e08f
Antoine B <alypios@mailfence.com>
2022-11-04 12:12:10 +0100

    CMakeLists.txt

--- src/Main.c
+++ src/Main.c
@@ -1,13 +1,11 @@
-#include <Menu.h>
-#include <Room.h>
-#include <Test.h>
-#include <Character.h>
-#include <Guard.h>
-#include <Relic.h>
+#include "Menu.h"
+#include "Room.h"
+#include "Test.h"
+#include "Character.h"
+#include "Guard.h"
+#include "Relic.h"
 #include "graphics/Graphic.h"
 #include <stdio.h>
-
-
 
 int main(int args, char *argv[]){
     printf("Working properly \n");
